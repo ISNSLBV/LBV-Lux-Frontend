5e3381227ad166d47ad227a7e7161aeefa0fce40
guzgoldman <hectorguzmanbz@gmail.com>
2025-06-13 21:37:03 -0300

    Componente PerfilCard agregado | Vista Perfil agregada

---
+++ src/screens/Perfil/Perfil.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PerfilCard from '../../components/PerfilCard/PerfilCard'
+
+const Perfil = () => {
+  return (
+    <div className='container'>
+        <PerfilCard />
+    </div>
+  )
+}
+
+export default Perfil
