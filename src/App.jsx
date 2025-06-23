2938f98bb7e843fc3191a37b99408029cf9eaff3
roGitHubAcc <romilu85@gmail.com>
2025-06-23 00:20:35 -0300

    Creación de página Preguntas Frecuentes (FAQ) | Ajuste de rutas públicas y estilos del componente desplegable

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,71 @@
-import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { AuthProvider } from './contexts/AuthContext'
-import Login from './screens/Login/Login'
-import Layout from './components/Layout/Layout'
-import Preinscripcion from './screens/Preinscripcion/Preinscripcion'
-import RutaPrivada from './components/RutaPrivada/RutaPrivada'
-import PanelAdministrador from './screens/Administrador/PanelAdministrador/PanelAdministrador'
-import Dashboard from './screens/Dashboard/Dashboard'
-import Perfil from './screens/Perfil/Perfil'
-import GestionPreinscriptos from './screens/Administrador/GestionPreinscriptos/GestionPreinscriptos'
-import GestionCarreras from './screens/Administrador/GestionCarreras/GestionCarreras'
-import GestionMaterias from './screens/Administrador/GestionMaterias/GestionMaterias'
-import GestionPlanes from './screens/Administrador/GestionPlanes/GestionPlanes'
-import { RutaPublica } from './components/RutaPublica/RutaPublica'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
+import Login from "./screens/Login/Login";
+import Layout from "./components/Layout/Layout";
+import Preinscripcion from "./screens/Preinscripcion/Preinscripcion";
+import RutaPrivada from "./components/RutaPrivada/RutaPrivada";
+import PanelAdministrador from "./screens/Administrador/PanelAdministrador/PanelAdministrador";
+import PreguntasFrecuentes from "./screens/PreguntasFrecuentes/PreguntasFrecuentes";
+import Dashboard from "./screens/Dashboard/Dashboard";
+import Perfil from "./screens/Perfil/Perfil";
+import GestionPreinscriptos from "./screens/Administrador/GestionPreinscriptos/GestionPreinscriptos";
+import GestionCarreras from "./screens/Administrador/GestionCarreras/GestionCarreras";
+import GestionMaterias from "./screens/Administrador/GestionMaterias/GestionMaterias";
+import GestionPlanes from "./screens/Administrador/GestionPlanes/GestionPlanes";
+import { RutaPublica } from "./components/RutaPublica/RutaPublica";
+import { RutaPublicaSinRedireccion } from "./components/RutaPublica/RutaPublicaSinRedireccion";
 
 function App() {
-
   return (
     <Router>
       <AuthProvider>
         <Routes>
           {/* Rutas públicas */}
-          <Route path='/preinscripcion' element={
-            <RutaPublica>
-              <Preinscripcion />
-            </RutaPublica>} />
-          <Route path='/login' element={
-            <RutaPublica>
-              <Login />
-            </RutaPublica>
-            } />
+          <Route
+            path="/preinscripcion"
+            element={
+              <RutaPublica>
+                <Preinscripcion />
+              </RutaPublica>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <RutaPublica>
+                <Login />
+              </RutaPublica>
+            }
+          />
+          <Route
+            path="/preguntas-frecuentes"
+            element={
+              <RutaPublicaSinRedireccion>
+                <PreguntasFrecuentes />
+              </RutaPublicaSinRedireccion>
+            }
+          />
           {/* Rutas privadas */}
           <Route element={<RutaPrivada />}>
             {/* Index */}
             <Route element={<Layout />}>
               {/* ---- Ruta usuario ---- */}
               <Route index element={<Dashboard />} />
-              <Route path='mi-perfil' element={<Perfil />} />
+              <Route path="mi-perfil" element={<Perfil />} />
               {/* ---- Ruta Administrador ---- */}
-              <Route path='admin' element={<RutaPrivada roles={['Administrador']} />}>
+              <Route
+                path="admin"
+                element={<RutaPrivada roles={["Administrador"]} />}
+              >
                 <Route index element={<PanelAdministrador />} />
-                <Route path='gestion-preinscriptos' element={<GestionPreinscriptos />} />
-                <Route path='gestion-carreras' element={<GestionCarreras />} />
-                <Route path='gestion-planes' element={<GestionPlanes />} />
-                <Route path='gestion-materias' element={<GestionMaterias />} />
+                <Route
+                  path="gestion-preinscriptos"
+                  element={<GestionPreinscriptos />}
+                />
+                <Route path="gestion-carreras" element={<GestionCarreras />} />
+                <Route path="gestion-planes" element={<GestionPlanes />} />
+                <Route path="gestion-materias" element={<GestionMaterias />} />
               </Route>
             </Route>
           </Route>
@@ -51,7 +73,7 @@ function App() {
         </Routes>
       </AuthProvider>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/RutaPublica/RutaPublicaSinRedireccion.jsx
@@ -0,0 +1,12 @@
+import { useAuth } from "../../contexts/AuthContext";
+import LinearProgress from "@mui/material/LinearProgress";
+
+export const RutaPublicaSinRedireccion = ({ children }) => {
+  const { checking } = useAuth(); // Solo necesitamos 'checking', no 'user' aquí
+
+  if (checking)
+    return (
+      <LinearProgress color="secondary" sx={{ height: 4, borderRadius: 2 }} />
+    );
+  return children;
+};
