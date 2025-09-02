81d51f64b79fa3953a119782675ac7383271aa55
guzgoldman <hectorguzmanbz@gmail.com>
2025-09-02 15:57:32 -0300

    Estadística de materias removida

--- src/screens/Perfil/Perfil.jsx
+++ src/screens/Perfil/Perfil.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import api from "../../api/axios";
 import Estadistica from "../../components/Perfil/Estadistica/Estadistica";
-import { TrendingUp, BookOpen, Award, Clock } from "lucide-react";
+import { TrendingUp, Award, Clock } from "lucide-react";
 import BarraSecciones from "../../components/Perfil/BarraSecciones/BarraSecciones";
 import Informacion from "../../components/Perfil/Informacion/Informacion";
 import styles from "./Perfil.module.css";
@@ -17,7 +17,6 @@ const fetchPerfil = async ({ queryKey }) => {
 
 const iconMap = {
   promedio: TrendingUp,
-  materias: BookOpen,
   aprobadas: Award,
   asistencia: Clock,
 };
@@ -59,7 +58,7 @@ const Perfil = () => {
 
       <div className={styles.estadisticas}>
         {estadisticas.map(({ iconoKey, valor }) => {
-          const Icon = iconMap[iconoKey] || TrendingUp;
+          const Icon = iconMap[iconoKey];
           const label = iconoKey.charAt(0).toUpperCase() + iconoKey.slice(1);
           return (
             <Estadistica
