676d1ba119a5555703f90aa222c8de87beb36c93
Héctor Guzmán <107734309+guzgoldman@users.noreply.github.com>
2025-09-17 13:13:14 -0300

    feat: Update base href and router basename for consistent routing

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
-  base: '/alumnos2025',
+  plugins: [react()],
+  base: "/alumnos2025/",
   css: {
     modules: {
-      generateScopedName:
-      "[name]__[local]___[hash:base64:5]",
-    }
-  }
-})
+      generateScopedName: "[name]__[local]___[hash:base64:5]",
+    },
+  },
+  build: {
+    outDir: "build",
+    assetsDir: "assets",
+  },
+});
