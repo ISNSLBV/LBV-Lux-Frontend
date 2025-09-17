3c24ebf2761472f9b584634bb0e6f37d7c96eac4
Héctor Guzmán <107734309+guzgoldman@users.noreply.github.com>
2025-09-17 13:28:16 -0300

    feat: Remove base href for consistent routing across the application

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/alumnos2025/",
   css: {
     modules: {
       generateScopedName: "[name]__[local]___[hash:base64:5]",
